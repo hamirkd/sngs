dac5869607cb8623832697d064feea6af8fb28b4
DAO Hamadou <daohamadou@gmail.com>
2026-08-15 15:30:42 +0000

    -

--- app/raps/includes/header.php
+++ app/raps/includes/header.php
@@ -1,8 +1,7 @@
-<!-- <table style="width: 100%; border-bottom: dashed 1px black;padding:5px 5px 5px 5px;">
+<table style="width: 100%; border-bottom: dashed 1px black;padding:5px 5px 5px 5px;">
     <tr style="vertical-align: top;">
         <td style="text-align: left;width: 100%; vertical-align: top;border:none;">
             <img src="../../public/images/header-lina.png" alt="logo" style="height: 20mm; width: auto;"><br>
         </td>
     </tr>
-</table> -->
-<img src="../../public/images/header-lina.png" alt="logo" style="height: 20mm; width: auto;"><br>
+</table>
